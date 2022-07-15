ef7abf31ed83e4f4c5a48b45f03de0f048efdb85
Deepika <saideepika219@gmail.com>
2022-07-15 17:57:57 +0530

    COMMIT

---
+++ src/main/java/com/portal/demo.java
@@ -0,0 +1,13 @@
+package com.portal;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class demo {
+
+	public static void main(String[] args) {
+
+		String encode = new BCryptPasswordEncoder().encode("admin");
+		System.out.println(encode);
+	}
+
+}
